206a6f8917f9a3869fde772a85255b6cebe3a31b
Leonardo Gazola Medeiros <leonardo.gazolamedeiros@gmail.com>
2023-04-22 23:25:07 -0300

    Fazendo Rodar Aqui

--- Gerenciador/src/main/java/experimento/gerenciador/model/ConectorH2.java
+++ Gerenciador/src/main/java/experimento/gerenciador/model/ConectorH2.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class ConectorH2 {
 
     public String namedatabase = "FluxoCaixa";
-    private String url = "jdbc:h2:C:/Users/leona/OneDrive/Área de Trabalho/Trabalhos/Contabilizador_Vendas/Gerenciador/src/main/java/experimento/gerenciador/database/FluxoCaixa";
+    private String url = "jdbc:h2:C:/Users/leona/OneDrive/Área de Trabalho/Trabalhos/Gerenciador_Fluxo_de_Caixa/Gerenciador/src/main/java/experimento/gerenciador/database/FluxoCaixa";
     private String user = "usuario";
     private String password = "";
 
